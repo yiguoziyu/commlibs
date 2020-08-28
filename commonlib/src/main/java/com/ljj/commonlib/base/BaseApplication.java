f469fb90fa81a85f51f622e561a79b5cd5528bf7
yiguoziyu <1197044274@qq.com>
2020-08-28 17:48:28 +0800

    设置baseurl

--- commonlib/src/main/java/com/ljj/commonlib/base/BaseApplication.java
+++ commonlib/src/main/java/com/ljj/commonlib/base/BaseApplication.java
@@ -5,18 +5,11 @@ import android.app.Application;
 public class BaseApplication extends Application {
 
     public static Application application;
-    private String baseUrl;
+    public static String baseUrl;
     @Override
     public void onCreate() {
         super.onCreate();
         application = this;
     }
 
-    public String getBaseUrl() {
-        return baseUrl == null ? "" : baseUrl;
-    }
-
-    public void setBaseUrl(String baseUrl) {
-        this.baseUrl = baseUrl;
-    }
 }
