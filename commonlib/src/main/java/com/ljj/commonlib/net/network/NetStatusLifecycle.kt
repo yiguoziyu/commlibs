dbc09f980371353d61791f3a0c590991737b206f
yiguoziyu <1197044274@qq.com>
2020-11-26 11:18:00 +0800

    修复NetStatusLifecycle内存泄漏

--- commonlib/src/main/java/com/ljj/commonlib/net/network/NetStatusLifecycle.kt
+++ commonlib/src/main/java/com/ljj/commonlib/net/network/NetStatusLifecycle.kt
@@ -7,34 +7,42 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import java.lang.ref.WeakReference
 import java.util.*
 
 object NetStatusLifecycle : DefaultLifecycleObserver {
     private const val TAG = "NetStatusLifecycle"
-    private var mNetBroadcastReceiver: NetBroadcastReceiver? = null
-    override fun onResume(owner: LifecycleOwner) {
-        super.onResume(owner)
+    private var receiver: WeakReference<NetBroadcastReceiver>? = null
+    override fun onCreate(owner: LifecycleOwner) {
+        super.onCreate(owner)
         try {
-            mNetBroadcastReceiver = NetBroadcastReceiver()
-            val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
-            if (owner is AppCompatActivity) {
-                owner.registerReceiver(mNetBroadcastReceiver, filter)
-            } else if (owner is Fragment) {
-                Objects.requireNonNull(owner.activity)?.registerReceiver(mNetBroadcastReceiver, filter)
+            val mNetBroadcastReceiver = NetBroadcastReceiver()
+            receiver = WeakReference<NetBroadcastReceiver>(mNetBroadcastReceiver)
+            receiver?.apply {
+                val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+                if (owner is AppCompatActivity) {
+                    owner.registerReceiver(get(), filter)
+                } else if (owner is Fragment) {
+                    Objects.requireNonNull(owner.activity)
+                        ?.registerReceiver(get(), filter)
+                }
             }
         } catch (e: Exception) {
             Log.e(TAG, "onResume==>e:${e.message}")
         }
     }
 
+
     override fun onPause(owner: LifecycleOwner) {
         super.onPause(owner)
         try {
-            if (owner is AppCompatActivity) {
-                owner.unregisterReceiver(mNetBroadcastReceiver)
-            } else if (owner is Fragment) {
-                Objects.requireNonNull(owner.activity)
-                        ?.unregisterReceiver(mNetBroadcastReceiver)
+            receiver?.apply {
+                if (owner is AppCompatActivity) {
+                    owner.unregisterReceiver(get())
+                } else if (owner is Fragment) {
+                    Objects.requireNonNull(owner.activity)
+                        ?.unregisterReceiver(get())
+                }
             }
         } catch (e: Exception) {
             Log.e(TAG, "onPause==>e:${e.message}")
